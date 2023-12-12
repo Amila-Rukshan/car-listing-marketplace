727bf49e2090b8a124952b90f24413efe7a5b0c6
amila <amila.15@cse.mrt.ac.lk>
2023-12-12 20:29:29 +0530

    encrypted passwords using bcrypt
    Signed-off-by: amila <amila.15@cse.mrt.ac.lk>

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,45 @@
+const bcrypt = require('bcrypt');
 const { StatusCodes } = require("http-status-codes");
 const { ROLES } = require("../config/consts/role");
 const { generate_access_token } = require("../utils/auth");
 
 const register = (req, res) => {
-  req.db.query(
-    "INSERT INTO user (username, email, password_hash, role_id) VALUES (?, ?, ?, ?)",
-    [req.body.username, req.body.email, req.body.password, ROLES.USER],
-    (err, result) => {
-      req.db.release();
-      if (err) {
-        if (err.code === "ER_DUP_ENTRY") {
-          res.status(StatusCodes.CONFLICT).send({
-            message: "Usename/email already exists!!",
-          });
-          return;
-        } else {
-          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-            message: "Internal server error",
-          });
-        }
-        return;
-      }
-      res.status(StatusCodes.OK).send({
-        message: "User registered successfully!",
+  bcrypt.hash(req.body.password, 10, function (err, password_hash) {
+    if (err) {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        message: "Error hashing password",
       });
+      return;
     }
-  );
+    req.db.query(
+      "INSERT INTO user (username, email, password_hash, role_id) VALUES (?, ?, ?, ?)",
+      [req.body.username, req.body.email, password_hash, ROLES.USER],
+      (err, result) => {
+        req.db.release();
+        if (err) {
+          if (err.code === "ER_DUP_ENTRY") {
+            res.status(StatusCodes.CONFLICT).send({
+              message: "Usename/email already exists!!",
+            });
+            return;
+          } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+              message: "Internal server error",
+            });
+          }
+          return;
+        }
+        res.status(StatusCodes.OK).send({
+          message: "User registered successfully!",
+        });
+      }
+    );
+  });
 };
 
 const login = (req, res) => {
   req.db.query(
-    "SELECT * FROM user WHERE email = ? AND password_hash = ?",
+    "SELECT * FROM user WHERE email = ?",
     [req.body.email, req.body.password],
     (err, rows) => {
       req.db.release();
@@ -47,8 +56,30 @@ const login = (req, res) => {
           .send({ message: "Invalid email or password" });
         return;
       }
-      const access_token = generate_access_token(rows[0]);
-      res.send({ access_token });
+
+      if (rows.length > 0) {
+        const user = rows[0];
+        bcrypt.compare(
+          req.body.password,
+          user.password_hash,
+          function (err, result) {
+            if (result == true) {
+              const access_token = generate_access_token(rows[0]);
+              res.status(StatusCodes.OK).send({
+                access_token,
+              });
+            } else {
+              res.status(StatusCodes.NOT_FOUND).send({
+                message: "Incorrect email/password!",
+              });
+            }
+          }
+        );
+      } else {
+        res.status(StatusCodes.NOT_FOUND).send({
+          message: "Incorrect email/password!!",
+        });
+      }
     }
   );
 };
